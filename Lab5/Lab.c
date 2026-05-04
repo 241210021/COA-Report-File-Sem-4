36e7a96bdefcb14cc5c5f1e0a63080d774c9cc54
241210021 <241210021@nitdelhi.ac.in>
2026-05-04 12:21:32 +0530

    Update Lab.c

--- Lab5/Lab.c
+++ Lab5/Lab.c
@@ -1,23 +1,47 @@
-// Experiment 5 – Part B
-// Addressing Modes using GDB (with Global Variable)
-// Name: Anshul Modi | Roll No: 241210021
-
 #include <stdio.h>
 
-int x = 10;   // Global variable (stored in data segment)
+int x = 10;
 
 int main(){
+
     int a=10, b=20;
     int result;
+
     int arr[3]={1,2,3};
+
     int *ptr;
 
-    result = a + 5;      // Immediate addressing
-    result = a + b;      // Direct addressing
-    result = x;          // Global variable (data segment access)
-    ptr = &a;            // Indirect addressing (load address)
-    result = *ptr;       // Indirect addressing (dereference)
-    result = arr[1];     // Indexed addressing
-    printf("Result : %d", result);
+    // Existing Modes
+    result = a + 5;      // Immediate
+    result = a + b;      // Direct
+    result = x;          // Global/Data segment
+
+    ptr = &a;
+    result = *ptr;       // Indirect
+
+    result = arr[1];     // Indexed
+
+
+    // 7. Base Register Addressing
+    int *base = arr;
+    result = *(base + 2);
+
+
+    // 8. Relative Addressing
+    goto label;
+
+    result = 999;
+
+label:
+    result = result + 1;
+
+
+    // 11. Auto Increment Addressing
+    ptr = arr;
+
+    result = *ptr++;
+
+    printf("Result : %d\n", result);
+
     return 0;
 }
